08f8ca3953855547e6242279c6d0b6ea6eb3c996
Thorsten Wilmer <wilmer.thorsten@gmail.com>
2015-03-21 21:52:14 +0530

    There seems to be a slightly different format with a tt namespace, this converter is supporting this

---
+++ subtitle.tt.sub.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+use XML::Simple;
+use Data::Dumper;
+sub toFrames {
+   my $time = shift;
+   $time=~s/\./:/g;
+ # print "Time is $time\n";
+   @items=split(/:/,$time);
+#print Dumper @items;
+#  print "fettich\n";
+#offset should be give by commandline
+$hours=$items[0]+0-10;
+$minutes=$items[1]+0;
+$seconds=$items[2]+0;
+$mseconds=$items[3]+0;
+#fps should come from XML
+$fps=25;
+$msperframe=1000/$fps;
+$mstime=((($hours*60)+$minutes)*60+$seconds)*1000+$mseconds;
+#print "ms $mstime\n";
+$frame=$mstime/$msperframe;
+# print "frame $frame\n";
+return $frame;
+}
+
+sub is_array {
+  my ($ref) = @_;
+  # Firstly arrays need to be references, throw
+  #  out non-references early.
+  return 0 unless ref $ref;
+
+  # Now try and eval a bit of code to treat the
+  #  reference as an array.  If it complains
+  #  in the 'Not an ARRAY reference' then we're
+  #  sure it's not an array, otherwise it was.
+  eval {
+    my $a = @$ref;
+  };
+  if ($@=~/^Not an ARRAY reference/) {
+    return 0;
+  } elsif ($@) {
+    die "Unexpected error in eval: $@\n";
+  } else {
+    return 1;
+  }
+
+}
+
+$in=shift;
+$out=shift;
+
+open(OUT,">$out") or die "Could not write $out\n";
+
+$xml = new XML::Simple;
+
+$data = $xml->XMLin($in) or die "Could not read $in\n";
+$styles=$data->{'tt:head'}->{'tt:styling'}->{'tt:style'};
+$items=$data->{'tt:body'}->{'tt:div'}->{'tt:p'};
+
+#print Dumper($items);
+
+for my $curItem ( @{ $items} )
+{
+  
+ 
+
+  $begin=$curItem->{begin};
+  $end=$curItem->{end};
+  
+  $content=$curItem->{'tt:span'};
+ print  Dumper $content;
+  if(is_array($content))
+  { 
+    $text="";
+    for $a (@{$content})
+    { 
+       $text=$text . "|".$a->{content};
+    }
+    $content=$text;
+  }else{
+       $content=$content->{content};
+  }
+  #   print $begin. "\n";
+$startFrame=   toFrames($begin);
+$endFrame= toFrames($end);
+
+ print OUT "{$startFrame}{$endFrame}$attr$content\n";
+
+}
